d81d7d65943259b1a6fadd7b1a4dbbdc60d89c07
spekode <jeffroberts1@gmail.com>
2012-12-04 00:35:14 -0800

    entity physics mumbojumbo, changes to build

--- entity.h
+++ entity.h
@@ -6,24 +6,36 @@
 #include "sprite.h"
 
 const cpFloat defaultMass = INFINITY;
-const cpFloat defaultInertia = INFINITY;
+const cpFloat defaultMoment = INFINITY;
 
 class Entity: public Tickable {
     private:
         /* physics */
         cpFloat mass;
-        cpFloat inertia;
+        cpFloat moment;
         cpBody *body;
         cpShape *shape;
+
+        bool physicsEnabled;
+
+        /* shared by constructors */
+        void sharedInit(cpFloat m, cpFloat moi);
 
     public:
         BaseSprite *sprite;
         Entity ();
         Entity (cpFloat m, cpFloat i);
-        ~Entity();
+        ~Entity ();
 
-        cpFloat getInertia();
-        cpFloat getMass();
+        void disablePhysics();
+        void enablePhysics(cpSpace *space);
+
+        cpFloat getMass() { return mass; }
+        cpFloat getMoment() { return moment; }
+
+        bool setShape(cpShape *sh);
+
+        void setSprite(BaseSprite *sp) { sprite = sp; }
 };
 
 #endif
